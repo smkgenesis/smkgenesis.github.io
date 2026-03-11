2b020ce1a98dc71c1af5d015351b258c61ec8465
Song Minkyu <smkgenesiskr@gmail.com>
2026-03-11 22:58:08 +0900

    Apply Ink Lab dark research style

--- run_in_venv.bat
+++ run_in_venv.bat
@@ -1,11 +1,4 @@
 @echo off
-cd /d %~dp0
-
-if not exist .venv\Scripts\activate.bat (
-  echo [ERROR] .venv not found at %cd%\.venv
-  echo Create it first: py -3 -m venv .venv
-  exit /b 1
-)
-
+cd /d C:\Users\smkge\Develop\smkgenesis.github.io
 call .venv\Scripts\activate.bat
 cmd /k
